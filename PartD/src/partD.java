e886227f897c267c7e313e341c0b464ed513de58
bethemf <be_fulford@yahoo.co.nz>
2019-05-09 09:58:47 +1200

    3rd commit

--- PartD/src/partD.java
+++ PartD/src/partD.java
@@ -4,6 +4,9 @@ public class partD {
 	public static void main(String[] args) {
 		
 		System.out.println("Hi my name is Beth");
+		System.out.println("My favourite colour is navy blue");
+		System.out.println("My favourite mode of transportation is a car");
+		
 	}
 
 }
